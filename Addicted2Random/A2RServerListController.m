2fd981443ed4ec980007a2c41127fe6b5353ee7c
Roman Gille <developer@romangille.com>
2013-04-22 15:29:54 +0200

    Connection with jamlist server

--- Addicted2Random/A2RServerListController.m
+++ Addicted2Random/A2RServerListController.m
@@ -10,9 +10,12 @@
 #import "SRWebSocket.h"
 
 
-@interface A2RServerListController () <UITableViewDataSource, UITableViewDelegate>
+@interface A2RServerListController () <UITableViewDataSource, UITableViewDelegate, SRWebSocketDelegate> {
+    BOOL _waitingForServerResponse;
+}
 
 @property (nonatomic, strong) NSArray *serverList;
+@property (nonatomic, strong) SRWebSocket *socket;
 
 @end
 
@@ -26,6 +29,7 @@ static NSString* kA2RServerListKey = @"a2rServerList";
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     self.title = NSLocalizedString(@"Serverliste", @"Title of the Server list view");
+    _waitingForServerResponse = NO;
     self.serverList = [[NSUserDefaults standardUserDefaults] arrayForKey:kA2RServerListKey];
     if (!_serverList.count) {
         NSString *serversFilePath = [[NSBundle mainBundle] pathForResource:@"A2RServers" ofType:@"plist"];
@@ -38,6 +42,13 @@ static NSString* kA2RServerListKey = @"a2rServerList";
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)dealloc {
+    self.serverList = nil;
+    self.socket = nil;
+}
+
+#pragma mark - TableView stuff
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
@@ -55,15 +66,52 @@ static NSString* kA2RServerListKey = @"a2rServerList";
     }
     
     NSDictionary *serverDict = (NSDictionary*)_serverList[indexPath.row];
-    
     cell.textLabel.text = serverDict[@"name"];
     cell.detailTextLabel.text = serverDict[@"description"];
+    
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    UIActivityIndicatorView *throbber = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    [throbber startAnimating];
+    cell.accessoryView = throbber;
+    _waitingForServerResponse = YES;
+    
+    NSDictionary *serverDict = (NSDictionary*)_serverList[indexPath.row];
+    self.socket = [[SRWebSocket alloc] initWithURL:[NSURL URLWithString:serverDict[@"address"]]];
+    _socket.delegate = self;
+    [_socket open];
 }
+
+#pragma mark - SRWebSocket delegate
+
+- (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message {
+    NSLog(@"message: %@", message);
+}
+
+- (void)webSocketDidOpen:(SRWebSocket *)webSocket {
+    NSLog(@"Socket to %@ opened", webSocket.url.host);
+    NSDictionary *message = @{@"jsonrpc": @"2.0",
+                              @"method": @"jams.getAll",
+                              @"id": [NSNumber numberWithInt:1]};
+    NSError *error;
+    NSData *data = [NSJSONSerialization dataWithJSONObject:message options:0 error:&error];
+    
+    [webSocket send:[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]];
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error {
+    NSLog(@"Could not conntec to %@ because of %@", webSocket.url.host, error);
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean {
+    NSLog(@"Closed connection to %@ with code %i because of %@", webSocket.url.host, code, reason);
+    NSLog(@"This was %@ clean close.", (wasClean ? @"a" : @"NO"));
+}
+
 
 
 @end
